1be52c0d1abd78bb2b94741a42e60ac0aa392edb
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-02 21:37:08 +0530

    Update ML-ANALYZER.py

--- ML-DETECTOR/ML-ANALYZER.py
+++ ML-DETECTOR/ML-ANALYZER.py
@@ -7,7 +7,7 @@ disease_model = DiseaseModel()
 disease_model.load_xgboost('ML-DETECTOR/model/xgboost_model.json')  # Replace with the correct model path
 
 # Set page width to wide
-st.set_page_config(layout='center')
+st.set_page_config(layout='collapsed')
 
 # Title
 st.write('# Disease Prediction using Machine Learning')
