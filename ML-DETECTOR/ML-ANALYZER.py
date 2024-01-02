8a47d6e1b17f9b1923b86f5197de82d9def45967
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-02 13:09:13 +0530

    Add files via upload

---
+++ ML-DETECTOR/ML-ANALYZER.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from code.DiseaseModel import DiseaseModel
+from code.helper import prepare_symptoms_array
+
+# Create disease class and load ML model
+disease_model = DiseaseModel()
+disease_model.load_xgboost('C:/Users/Jilshad M/Desktop/Project/MEDI-CHATBOT/ML-DETECTOR/model/xgboost_model.json')
+
+# Set page width to wide
+st.set_page_config(layout='wide')
+
+
+# Title
+st.write('# Disease Prediction using Machine Learning')
+
+symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
+
+X = prepare_symptoms_array(symptoms)
+
+# Trigger XGBoost model
+if st.button('Predict'): 
+    # Run the model with the python script
+    
+    prediction, prob = disease_model.predict(X)
+    st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
+
+
+    tab1, tab2= st.tabs(["Description", "Precautions"])
+
+    with tab1:
+        st.write(disease_model.describe_predicted_disease())
+
+    with tab2:
+        precautions = disease_model.predicted_disease_precautions()
+        for i in range(4):
+            st.write(f'{i+1}. {precautions[i]}')
