b841fb7eac6b65060f29db686499c4cd3bb782bc
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-03 21:46:29 +0530

    Update ML-ANALYZER.py

--- ML-ANALYZER.py
+++ ML-ANALYZER.py
@@ -30,40 +30,6 @@ st.markdown("""
 
 # Chatbot Functionality
 def chatbot_page():
-    # Chatbot code here
-
-# Disease Prediction Functionality
-def disease_prediction_page():    
-    # Create disease class and load ML model
-    disease_model = DiseaseModel()
-    disease_model.load_xgboost('model/xgboost_model.json')  # Replace with the correct model path
-    
-    st.sidebar.write('Disease Prediction using Machine Learning')
-    # Title
-    st.write('# Disease Prediction using Machine Learning')
-    
-    # Get symptoms from user input
-    symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
-    
-    X = prepare_symptoms_array(symptoms)  # Assuming prepare_symptoms_array handles symptom data
-    
-    # Trigger XGBoost model
-    if st.button('Predict'): 
-        # Run the model prediction
-        prediction, prob = disease_model.predict(X)  # Assuming predict() returns prediction and probability
-        
-        st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
-    
-        st.write(disease_model.describe_predicted_disease())
-        st.text("")
-        st.write("## Precautions:")
-        precautions = disease_model.predicted_disease_precautions()
-        for i in range(4):
-            st.write(f'{i+1}. {precautions[i]}')
-
-
-# Diagnostic Analyzer Functionality
-def diagnostic_analyzer_page():
     def get_initial_message():
     messages=[
             {"role": "system", "content": "You are a helpful Medical Diagnostic AI Doctor. Who anwers brief questions about Diseases, Symptomps and medical findings."},
@@ -131,6 +97,101 @@ def diagnostic_analyzer_page():
     if st.session_state['generated']:
         display_chat_history()
 
+
+# Disease Prediction Functionality
+def disease_prediction_page():    
+    # Create disease class and load ML model
+    disease_model = DiseaseModel()
+    disease_model.load_xgboost('model/xgboost_model.json')  # Replace with the correct model path
+    
+    st.sidebar.write('Disease Prediction using Machine Learning')
+    # Title
+    st.write('# Disease Prediction using Machine Learning')
+    
+    # Get symptoms from user input
+    symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
+    
+    X = prepare_symptoms_array(symptoms)  # Assuming prepare_symptoms_array handles symptom data
+    
+    # Trigger XGBoost model
+    if st.button('Predict'): 
+        # Run the model prediction
+        prediction, prob = disease_model.predict(X)  # Assuming predict() returns prediction and probability
+        
+        st.write(f'## Disease: {prediction} with {prob*100:.2f}% probability')
+    
+        st.write(disease_model.describe_predicted_disease())
+        st.text("")
+        st.write("## Precautions:")
+        precautions = disease_model.predicted_disease_precautions()
+        for i in range(4):
+            st.write(f'{i+1}. {precautions[i]}')
+
+
+# Diagnostic Analyzer Functionality
+def diagnostic_analyzer_page():
+    # Streamlit app title and description
+    st.markdown("<h1 style='text-align: center;'>Medical Diagnostic AI-Analyzer</h1>", unsafe_allow_html=True)
+    st.markdown("<p style='text-align: center;'>Enter patient information to receive a diagnostic recommendation</p>", unsafe_allow_html=True)
+    st.divider()
+    
+    
+    col1, col2, col3 = st.columns(3, gap="large")
+    
+    # User inputs
+    with col1:
+        gender = st.radio("Gender", ["Male", "Female"])
+    with col2:    
+        pregnancy = st.radio("Pregnancy", ["No", "Yes"]) if gender == "Female" else "No"
+    with col3:
+        age = st.number_input("Age", min_value=0, max_value=99, step=1)
+    
+    context = st.text_input("Patient's Background *(Example: gone to an outdoor music festival in north america, shared drinks and cigarettes with friends with similar symptoms)*", placeholder="none", max_chars=500 ,help=":green[**Enter patient's known background information, including their past medical conditions, medications, family history, lifestyle, and other relevant information that can help in diagnosis and treatment**]")
+    symptoms = st.text_input("Symptoms *(Example: high-grade fever, lethargy, headache, and abdominal pain for two days)*", placeholder="none", max_chars=500 ,help=":green[**List all symptoms indicating the presence of an underlying medical condition**]")
+    exam_findings = st.text_input("Examination Findings *(Example: petechial lesions on the palms of his hands and feet, bug bites)*", placeholder="none", max_chars=500, help=":green[**List all the information gathered through visual inspection, palpation, percussion, and auscultation during the examination**]")
+    lab_results = st.text_input("Laboratory Test Results *(Example: w/IgE levels > 3000 IU/m)*", placeholder="none", max_chars=500, help=":green[**List output of tests performed on samples of bodily fluids, tissues, or other substances to help diagnose, monitor, or treat medical conditions. These tests can include blood tests, urine tests, imaging tests, biopsies, and other diagnostic procedures**]")
+    
+    # Combine user inputs into a report
+    report_list = [
+        f"Gender: {gender}",
+        f"Age: {age}",
+        f"Pregnancy: {pregnancy}" if gender == "Female" else "",
+        context,
+        symptoms,
+        exam_findings,
+        lab_results
+    ]
+    report = "\n".join([f"{i + 1}. {report_list[i]}" for i in range(len(report_list)) if report_list[i] != "none"])
+    
+    # Button to trigger the diagnostic analysis
+    if st.button("Analyze"):
+        if symptoms != "none":
+            # Add a spinner while generating the diagnostic recommendation
+            with st.spinner("Generating diagnostic details..."):
+                # Make API call to OpenAI
+                response = openai.Completion.create(
+                    engine="text-davinci-003",  # Use Codex engine for code-based tasks
+                    prompt=f"Patient with the following information:\n\n{report}\n\nProvide diagnostic name, precautions and a large brief explanation about the found diagnostic.",
+                    max_tokens=300
+                )
+    
+            # Display the diagnostic recommendation
+            st.subheader("Predicted Diagnostic Details:")
+            st.write(response["choices"][0]["text"])
+    
+            # Display a note
+            st.divider()
+            st.info(
+                "Disclaimer: The diagnostic recommendation provided here is generated based on the information provided "
+                "and is not a substitute for professional medical advice. It is important to note that this system does not "
+                "guarantee 100% accuracy. Consultation with a qualified healthcare professional is strongly recommended for "
+                "a precise and reliable diagnosis. Your health and well-being are of utmost importance, and a medical expert "
+                "can provide personalized guidance based on a thorough examination of your specific situation."
+            )
+            st.divider()
+        else:
+            st.warning("Please enter symptoms before analyzing.")
+        
 # Sidebar navigation
 page = st.sidebar.selectbox("Select Page", ("Chatbot", "Disease Prediction", "Diagnostic Analyzer"))
 
