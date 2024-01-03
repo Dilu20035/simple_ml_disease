fdac219613971a56c9a74f047b701802275697e2
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-03 08:05:26 +0530

    Update ML-ANALYZER.py

--- ML-ANALYZER.py
+++ ML-ANALYZER.py
@@ -3,7 +3,6 @@ from code.DiseaseModel import DiseaseModel  # Ensure the correct import path
 from code.helper import prepare_symptoms_array  # Ensure the correct import path
 
 
-st.set_page_config(page_title="HDA-ML-Analyzer", page_icon=None, layout="centered")
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
 st.markdown("""
