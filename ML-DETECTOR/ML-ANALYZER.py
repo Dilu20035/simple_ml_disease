8332ddfdeabc7055547ce00debfa180675189dc8
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-03 07:14:29 +0530

    Update ML-ANALYZER.py

--- ML-DETECTOR/ML-ANALYZER.py
+++ ML-DETECTOR/ML-ANALYZER.py
@@ -4,7 +4,6 @@ from code.helper import prepare_symptoms_array  # Ensure the correct import path
 import requests
 from streamlit_lottie import st_lottie
 
-st.set_page_config(page_title="HDA-ML-Analyzer", page_icon=None, layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 
