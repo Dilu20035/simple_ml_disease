6a395f379e2a6bf2e1bc278d6ae9891505ed4b0e
Jilshad M <93209693+Dilu20035@users.noreply.github.com>
2024-01-07 14:52:25 +0530

    Update ML-ANALYZER.py

--- ML-ANALYZER.py
+++ ML-ANALYZER.py
@@ -129,7 +129,7 @@ disease_model.load_xgboost('model/xgboost_model.json')  # Replace with the corre
 
 st.title('')
 # Streamlit App Title
-st.markdown("<h1 style='text-align: center;'>Disease Prediction using Machine Learning</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Disease Prediction using ML (Machine Learning)</h1>", unsafe_allow_html=True)
 
 # Get symptoms from user input
 symptoms = st.multiselect('What are your symptoms?', options=disease_model.all_symptoms)
